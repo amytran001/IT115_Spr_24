a452d838d3af61af75af3d5dbd372c81f6fbefb8
amytran001 <amyntran007@gmail.com>
2024-05-14 15:59:52 -0700

    First push

---
+++ json.py
@@ -0,0 +1,11 @@
+#
+import json
+
+data = {
+
+    'name': 'John Doe',
+    'age': 23,
+    'city': 'New York, NY',
+    'intrests': ['Volleyball','Drawing','Gaming','Running'],
+    'is_student': False
+}
